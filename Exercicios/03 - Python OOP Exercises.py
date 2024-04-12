f8e0dd59872a1296628bcee6bc7cf907f62580a9
gabrielsoaresceravolo <gabriel.striderws@gmail.com>
2024-04-12 16:10:13 -0300

    Organizing files and adding a new .ipynb explaining Object Orientation in Python (Organized project files for improved structure and readability; Added a new notebook (.ipynb) explaining the principles and concepts of Object Orientation in Python)

---
+++ Exercicios/03 - Python OOP Exercises.py
@@ -0,0 +1,12 @@
+# 1) Na disciplina de Modelagem de Padrões de Projetos você e seu grupo estão desenvolvimento um
+# Sistema WEB que faz uso de Orientação à Objetos. Implemente em Python todas as Classes da camada
+# de negócio existentes em seu projeto.
+
+class Users:
+
+    def __init__(self, nome, email, senha):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+# Em breve
